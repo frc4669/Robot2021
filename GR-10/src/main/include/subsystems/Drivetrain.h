c7fdb6a392f90ec4462df531f15f5a1aa0c61764
Iakov Taranenko <48700922+iatara@users.noreply.github.com>
2022-01-19 17:38:38 -0800

    WPI_TalonFX constructor fix for new 2022 update

--- GR-10/src/main/include/subsystems/Drivetrain.h
+++ GR-10/src/main/include/subsystems/Drivetrain.h
@@ -5,8 +5,14 @@
 #pragma once
 
 #include <frc2/command/SubsystemBase.h>
+#include <frc/SpeedController.h>
+#include <frc/drive/DifferentialDrive.h>
+#include <frc/SpeedControllerGroup.h>
 
-#include <frc/ADIS16470_IMU.h>
+#include <ctre/Phoenix.h>       // talon
+#include <frc/ADIS16470_IMU.h>  // imu
+
+#include <Constants.h>
 
 class Drivetrain : public frc2::SubsystemBase {
  public:
@@ -21,5 +27,18 @@ class Drivetrain : public frc2::SubsystemBase {
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
 
-  frc::ADIS16470_IMU m_imu;
+  // motors
+  WPI_TalonFX m_leftMaster  = {DriveConstants::kLeftMotor1Port};
+  WPI_TalonFX m_leftSlave   = {DriveConstants::kLeftMotor2Port};
+  WPI_TalonFX m_rightMaster = {DriveConstants::kRightMotor1Port};
+  WPI_TalonFX m_rightSlave  = {DriveConstants::kRightMotor2Port};
+
+  frc::SpeedControllerGroup m_leftMotors{m_leftMaster, m_leftSlave};
+  frc::SpeedControllerGroup m_rightMotors{m_rightMaster, m_rightSlave};
+
+  frc::DifferentialDrive m_drive{m_leftMotors, m_rightMotors};
+
+
+
+  frc::ADIS16470_IMU m_imu{ };
 };
