76b46157d28658efbcd4c126c61fd66f8c0a2c3f
iatara <48700922+iatara@users.noreply.github.com>
2022-02-19 14:22:27 -0800

    Add future layout

---
+++ src/main/cpp/commands/DeployIntake.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/DeployIntake.h"
+
+DeployIntake::DeployIntake() {
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void DeployIntake::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void DeployIntake::Execute() {}
+
+// Called once the command ends or is interrupted.
+void DeployIntake::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool DeployIntake::IsFinished() {
+  return false;
+}
---
+++ src/main/cpp/commands/RunIntake.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/RunIntake.h"
+
+RunIntake::RunIntake() {
+  // Use addRequirements() here to declare subsystem dependencies.
+}
+
+// Called when the command is initially scheduled.
+void RunIntake::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void RunIntake::Execute() {}
+
+// Called once the command ends or is interrupted.
+void RunIntake::End(bool interrupted) {}
+
+// Returns true when the command should end.
+bool RunIntake::IsFinished() {
+  return false;
+}
