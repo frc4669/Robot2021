df34fb7df593e949a12876b1b4ffbf031869bfab
iatara <48700922+iatara@users.noreply.github.com>
2022-03-18 01:21:29 -0700

    Climber shuffleboard data

--- src/main/cpp/subsystems/Climber.cpp
+++ src/main/cpp/subsystems/Climber.cpp
@@ -3,6 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 #include "subsystems/Climber.h"
+
 #include <frc/smartdashboard/SmartDashboard.h>
 
 Climber::Climber() {
@@ -12,12 +13,12 @@ Climber::Climber() {
 
 // This method will be called once per scheduler run
 void Climber::Periodic() {
-/*   frc::SmartDashboard::PutNumber("Right Climber Position", GetRightPostion());
+  frc::SmartDashboard::PutNumber("Right Climber Position", GetRightPostion());
   frc::SmartDashboard::PutNumber("Left Climber Position", GetLeftPosition());
 
-  frc::SmartDashboard::PutBoolean("Right Limit Switch", IsRightLimitHit());
-  frc::SmartDashboard::PutBoolean("Left Limit Switch", IsLeftLimitHit());
-  frc::SmartDashboard::PutBoolean("Arms Zeroed", AreArmsZeroed()); */
+  frc::SmartDashboard::PutBoolean("Right Limit Switch Triggered", IsRightLimitHit());
+  frc::SmartDashboard::PutBoolean("Left Limit Switch Triggered", IsLeftLimitHit());
+  frc::SmartDashboard::PutBoolean("Arms Zeroed", AreArmsZeroed());
 
   //if(AreArmsZeroed() == false)
     //ZeroArms();
@@ -39,11 +40,11 @@ void Climber::ZeroArms() {
   // Lower slave motor if not at base position
   //?: slave motor lowered first because if the master hits the limit switch, the slave will not be able to move
   if(!IsLeftLimitHit())
-    m_slaveMotor.Set(ControlMode::PercentOutput, 0.8);
+    m_slaveMotor.Set(ControlMode::PercentOutput, 0.2);
   
   // Lower master motor if not at base position once slave motor is at base position
   if(!IsRightLimitHit())
-    m_masterMotor.Set(ControlMode::PercentOutput, 0.8);
+    m_masterMotor.Set(ControlMode::PercentOutput, 0.2);
 
   // If both motors are at base position, stop the periodic loop, they have now been synchronized
   if(IsLeftLimitHit() && IsRightLimitHit()) {
@@ -70,8 +71,8 @@ double Climber::GetRightPostion() {
 }
 
 double Climber::GetLeftPosition() {
-  //?: abs is needed because the slave motor is inverted
-  return abs(m_slaveMotor.GetSensorCollection().GetIntegratedSensorPosition());
+  //?: invert is needed because the slave motor is inverted
+  return -m_slaveMotor.GetSensorCollection().GetIntegratedSensorPosition();
 }
 
 void Climber::ConfigureMotor(WPI_TalonFX &motor, bool inverted) {
