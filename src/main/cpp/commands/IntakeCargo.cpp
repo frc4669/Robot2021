9ab8cc8c52ad9a43f9afe3a10fd3613e82279a5f
iatara <48700922+iatara@users.noreply.github.com>
2022-03-18 01:53:23 -0700

    Create IntakeCargo command

---
+++ src/main/cpp/commands/IntakeCargo.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include "commands/IntakeCargo.h"
+
+IntakeCargo::IntakeCargo(Intake* intake) {
+  AddRequirements( {intake} );
+  this->intake = intake;
+}
+
+// Called when the command is initially scheduled.
+void IntakeCargo::Initialize() {}
+
+// Called repeatedly when this Command is scheduled to run
+void IntakeCargo::Execute() {
+  intake->RunIntake(false);
+  intake->RunFeeder(false);
+}
+
+// Called once the command ends or is interrupted.
+void IntakeCargo::End(bool interrupted) {
+  intake->StopIntake();
+  intake->StopFeeder();
+}
+
+// Returns true when the command should end.
+bool IntakeCargo::IsFinished() {
+  return false;
+}
