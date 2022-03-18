0cb0ab7f65864e75f4d06d454b95f2e00f76b720
iatara <48700922+iatara@users.noreply.github.com>
2022-03-18 02:27:51 -0700

    Add POV button support for f310 + clean up code

--- src/main/include/F310.h
+++ src/main/include/F310.h
@@ -32,17 +32,6 @@ class F310 {
  // Objects/Defined Variables
   frc::Joystick f310joystick{OperatorConstants::kF310};
 
-  const int green_button = 1;
-  const int red_button = 2;
-  const int blue_button = 3;
-  const int orange_button = 4;
-  const int left_shoulder_button = 5;
-  const int right_shoulder_button = 6;
-  const int back_button = 7;
-  const int start_button = 8;
-  const int left_joy_button = 9;
-  const int right_joy_button = 10;
-
   frc2::JoystickButton greenButtonObject{&f310joystick, green_button};
   frc2::JoystickButton redButtonObject{&f310joystick, red_button};
   frc2::JoystickButton blueButtonObject{&f310joystick, blue_button};
@@ -53,4 +42,26 @@ class F310 {
   frc2::JoystickButton startButtonObject{&f310joystick, start_button};
   frc2::JoystickButton leftJoyButtonObject{&f310joystick, left_joy_button};
   frc2::JoystickButton rightJoyButtonObject{&f310joystick, right_joy_button};
+
+  frc2::POVButton dpadUpButtonObject{&f310joystick, 0};
+  frc2::POVButton dpadUpperRightButtonObject{&f310joystick, 45};
+  frc2::POVButton dpadRightButtonObject{&f310joystick, 90};
+  frc2::POVButton dpadLowerRightButtonObject{&f310joystick, 135};
+  frc2::POVButton dpadDownButtonObject{&f310joystick, 180};
+  frc2::POVButton dpadLowerLeftButtonObject{&f310joystick, 225};
+  frc2::POVButton dpadLeftButtonObject{&f310joystick, 270};
+  frc2::POVButton dpadUpperLeftButtonObject{&f310joystick, 315};
+  frc2::POVButton dpadCenteredButtonObject{&f310joystick, -1};
+
+ private:
+  const int green_button = 1;
+  const int red_button = 2;
+  const int blue_button = 3;
+  const int orange_button = 4;
+  const int left_shoulder_button = 5;
+  const int right_shoulder_button = 6;
+  const int back_button = 7;
+  const int start_button = 8;
+  const int left_joy_button = 9;
+  const int right_joy_button = 10;
 };
